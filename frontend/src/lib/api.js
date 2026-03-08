d9d1f053c9846c05d7269efef30dda70711e6e20
tanespol916 <mek.tanespol@gmail.com>
2026-03-08 22:14:28 +0700

    Remove Vite proxy and use direct Railway API URL

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://taskflow-production-cede.up.railway.app/api',
 });
 
 api.interceptors.request.use((config) => {
